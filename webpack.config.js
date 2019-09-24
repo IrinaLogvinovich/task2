48614369100952352c68f75d13d5da3298045ad3
dev16 <dev16@idea-labs.ru editor = "C:\Programs\Microsoft VS Code\Code.exe" --wait>
2019-09-24 12:01:54 +0500

    корректировки в сборку в pug

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ let conf = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js',
-        publicPath: 'dist/'
+        // publicPath: 'dist/'
     },
     devServer: {
         overlay: true,
